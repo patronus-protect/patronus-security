16f041e1561ab96fb604740951dedaea4e31653c
Benedikt Veith <benedikt.veith@gmail.com>
2026-09-05 14:41:34 +0200

    Suppress external os-release shellcheck info

--- ark-coordinator/deploy/bootstrap.sh
+++ ark-coordinator/deploy/bootstrap.sh
@@ -34,7 +34,7 @@ PY
 
 install_docker() {
     if ! command -v docker >/dev/null; then
-        # shellcheck source=/etc/os-release
+        # shellcheck disable=SC1091 -- file exists on supported Debian/Ubuntu hosts.
         . /etc/os-release
         [[ $ID == ubuntu && $VERSION_ID == 24.04 ]] || die 'Automatic Docker installation requires Ubuntu 24.04.'
         apt-get update
